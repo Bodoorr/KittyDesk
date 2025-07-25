8a4d9af04f542f2026c87090fe235aad4cd83d65
Bodoor <Bodoor5@hotmail.com>
2025-07-26 00:02:49 +0300

    fixed db/index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
-const {MONGODB_URI}= require('../config')
+require('dotenv').config()
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI)
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
   } catch (err) {
     console.error('MongoDB connection error:', err.message)
     process.exit(1)
   }
 }
+const db = mongoose.connection
 
 module.exports = connectDB
